aac56a9f35fd47e7594eb485e90e0171fd830e7e
Angel <angelsanchez.as524@gmail.com>
2022-09-28 21:56:06 -0700

    added grid cards

--- client/src/components/species.js
+++ client/src/components/species.js
@@ -4,63 +4,62 @@ import Form from "./form";
 function Species() {
   const [species, setSpecies] = useState([]);
 
-
   const addSpecies = (newSpecies) => {
     setSpecies((species) => [...species, newSpecies]);
   };
 
   const deleteSpecies = async (deleteId) => {
     await fetch(`http://localhost:5000/api/species/${deleteId}`, {
-        method: "DELETE",
+      method: "DELETE",
     });
- 
+
     await getSpecies(); //
-    
-    
   };
-    const getSpecies = async () => {
+  const getSpecies = async () => {
     const response = await fetch(`http://localhost:5000/api/species`);
     const specie = await response.json();
     setSpecies(specie);
-    };
+  };
 
-    useEffect(() => {
-        getSpecies();
-    }, []);
+  useEffect(() => {
+    getSpecies();
+  }, []);
 
   return (
     <section className="species">
       <h2>List of Species</h2>
-      <table id="species">
+      <ul id="species" className="spec-cards">
         {species.map((species, index) => {
           return (
-            <tr key={index}>
-              <td>
+            
+            <li key={index} className="spec-card">
+              
                 <strong>Common Name:</strong>
                 {species.common_name}
-              </td>
-                <br />
-              <td>
+              
+              <br />
+              
                 <strong>Scientific Name:</strong>
                 {species.scientific_name}{" "}
-              </td>
+              
               <br />
-              <td>
+              
                 <strong>Population:</strong>
                 {species.population}
-              </td>
+              
               <br />
               <button>
                 <span
                   className="material-symbols-outlined"
-                  onClick={() => deleteSpecies(species.id)}>
+                  onClick={() => deleteSpecies(species.id)}
+                >
                   delete
                 </span>
               </button>
-            </tr>
+            </li>
           );
         })}
-      </table>
+      </ul>
       <Form addSpecies={addSpecies} />
     </section>
   );
